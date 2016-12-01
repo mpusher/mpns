2ac0b37e59c8c649529e5038fdd104448d639dea
夜色 <yanxx@shinemo.com>
2016-12-01 17:13:22 +0800

    mpush client 升级到0.0.6

--- mpns-web/src/main/java/com/mpush/mpns/web/AppLauncher.java
+++ mpns-web/src/main/java/com/mpush/mpns/web/AppLauncher.java
@@ -4,12 +4,14 @@ import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Launcher;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.SLF4JLogDelegateFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import static io.vertx.core.impl.launcher.commands.BareCommand.VERTX_OPTIONS_PROP_PREFIX;
 import static io.vertx.core.logging.LoggerFactory.LOGGER_DELEGATE_FACTORY_CLASS_NAME;
 
 /**
@@ -19,22 +21,30 @@ import static io.vertx.core.logging.LoggerFactory.LOGGER_DELEGATE_FACTORY_CLASS_
  */
 public class AppLauncher extends Launcher {
 
+    private JsonObject config;
+
     public static void main(String[] args) {
         AppLauncher launcher = new AppLauncher();
         launcher.dispatch(args);
     }
 
+    //first
+    @Override
+    public void afterConfigParsed(JsonObject config) {
+        super.afterConfigParsed(config);
+        this.config = config;
+    }
+
+    //second
     @Override
     public void beforeStartingVertx(VertxOptions options) {
         super.beforeStartingVertx(options);
         System.setProperty(LOGGER_DELEGATE_FACTORY_CLASS_NAME, SLF4JLogDelegateFactory.class.getName());
-        /*Properties cfg = new Properties();
-        try (InputStream in = AppLauncher.class.getResourceAsStream("cfg.properties");) {
-            cfg.load(in);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
+        options.setWarningExceptionTime(
+                config.getLong(VERTX_OPTIONS_PROP_PREFIX + "warningExceptionTime", options.getWarningExceptionTime())
+        );
     }
+
 
     @Override
     public void afterStartingVertx(Vertx vertx) {
